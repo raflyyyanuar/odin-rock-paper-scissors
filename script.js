95498a1c29a059c35f9e48c85334433d93ffa0b9
Rafly Yanuar <71368439+raflyyyanuar@users.noreply.github.com>
2024-02-06 22:24:02 +0700

    add playRound() and displayResult()

--- script.js
+++ script.js
@@ -11,3 +11,48 @@ function getComputerChoice() {
             return "Scissors"
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+    // Display result message depending on win, lose, or tie
+    function displayResult(status) {
+        if(status == 1) {
+            return "Player wins! " + playerSelection + " beats " + computerSelection
+        }
+        else if (status == -1) {
+            return "Computer wins! " + playerSelection + " loses to " + computerSelection
+        }
+        else if (status == 0) {
+            return "It's a tie! " + playerSelection + " can not beat " + computerSelection
+        }
+    }
+
+    let p = playerSelection.toLowerCase()
+
+    // handle every possible outcome
+    switch(true) {
+        // if player chooses Rock
+        case (p == "rock" && computerSelection == "Rock"):
+            return displayResult(0)
+        case (p == "rock" && computerSelection == "Paper"):
+            return displayResult(-1)
+        case (p == "rock" && computerSelection == "Scissors"):
+            return displayResult(1)
+        
+        // if player chooses Paper
+        case (p == "paper" && computerSelection == "Rock"):
+            return displayResult(1)
+        case (p == "paper" && computerSelection == "Paper"):
+            return displayResult(0)
+        case (p == "paper" && computerSelection == "Scissors"):
+            return displayResult(-1)
+        
+        // if player chooses Scissors
+        case (p == "scissors" && computerSelection == "Rock"):
+            return displayResult(-1)
+        case (p == "scissors" && computerSelection == "Paper"):
+            return displayResult(1)
+        case (p == "scissors" && computerSelection == "Scissors"):
+            return displayResult(0)
+    }
+}
