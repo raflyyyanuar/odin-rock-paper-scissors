0b0decdd0c7b2943fd6ff6a44ec41fe4ef05b3a2
Rafly Yanuar <71368439+raflyyyanuar@users.noreply.github.com>
2024-02-06 21:59:48 +0700

    add function getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+function getComputerChoice() {
+    // Generate a random number between 1 and 3
+    let choice = Math.floor( Math.random() * 3 )
+
+    switch(choice) {
+        case 0:
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2:
+            return "Scissors"
+    }
+}
