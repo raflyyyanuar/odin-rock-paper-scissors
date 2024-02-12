274f757e87f931aa8a3ca5f8312821754a3bcbfa
Rafly Yanuar <71368439+raflyyyanuar@users.noreply.github.com>
2024-02-12 21:52:29 +0700

    add ui

--- script.js
+++ script.js
@@ -1,68 +1,102 @@
 function getComputerChoice() {
-    // Generate a random number between 1 and 3
-    let choice = Math.floor( Math.random() * 3 )
-
-    switch(choice) {
-        case 0:
-            return "Rock"
-        case 1:
-            return "Paper"
-        case 2:
-            return "Scissors"
-    }
+    // Generate a random number between 0 and 2
+    // with 0 being rock, 1 being paper, and 2 being scissors
+    return Math.floor( Math.random() * 3 )
 }
 
-function playRound(playerSelection, computerSelection) {
-    
-    // Display result message depending on win, lose, or tie
-    function displayResult(status) {
-        if(status == 1) {
-            return "Player wins! " + playerSelection + " beats " + computerSelection
-        }
-        else if (status == -1) {
-            return "Computer wins! " + playerSelection + " loses to " + computerSelection
-        }
-        else if (status == 0) {
-            return "It's a tie! " + playerSelection + " can not beat " + computerSelection
-        }
+function playRound(p, c) {
+    if (p == c) {
+        return 0
     }
-
-    let p = playerSelection.toLowerCase()
 
     // handle every possible outcome
     switch(true) {
         // if player chooses Rock
-        case (p == "rock" && computerSelection == "Rock"):
-            return displayResult(0)
-        case (p == "rock" && computerSelection == "Paper"):
-            return displayResult(-1)
-        case (p == "rock" && computerSelection == "Scissors"):
-            return displayResult(1)
+        case (p == 0 && c == 1):
+            return -1
+        case (p == 0 && c == 2):
+            return 1
         
         // if player chooses Paper
-        case (p == "paper" && computerSelection == "Rock"):
-            return displayResult(1)
-        case (p == "paper" && computerSelection == "Paper"):
-            return displayResult(0)
-        case (p == "paper" && computerSelection == "Scissors"):
-            return displayResult(-1)
+        case (p == 1 && c == 0):
+            return 1
+        case (p == 1 && c == 2):
+            return -1
         
         // if player chooses Scissors
-        case (p == "scissors" && computerSelection == "Rock"):
-            return displayResult(-1)
-        case (p == "scissors" && computerSelection == "Paper"):
-            return displayResult(1)
-        case (p == "scissors" && computerSelection == "Scissors"):
-            return displayResult(0)
+        case (p == 2 && c == 0):
+            return -1
+        case (p == 2 && c == 1):
+            return 1
     }
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors?: ")
-        let computerSelection = getComputerChoice()
-        alert(playRound(playerSelection, computerSelection))
+function parseChoice(choice) {
+    let choices = ["Rock", "Paper", "Scissors"]
+    return choices[choice]
+}
+
+// Greet console with document header
+console.log(document.querySelector('h1').innerHTML)
+
+let btns = document.querySelectorAll('button')
+
+let playerScore = 0
+let computerScore = 0
+let drawScore = 0
+let round = 0
+
+const roundCounter = document.querySelector('#round')
+const playerCounter = document.querySelector('#player')
+const computerCounter = document.querySelector('#computer')
+const drawCounter = document.querySelector('#draw')
+
+const play = function() {
+    roundCounter.textContent = `Round: ${++round}`
+    
+    let choice = this.value
+    let playerSelection = choice
+    
+    let computerSelection = getComputerChoice();
+    
+    let result = playRound(playerSelection, computerSelection)
+    
+    
+    switch(result) {
+        case 0:
+            drawScore++
+            break
+        case 1:
+            playerScore++
+            break
+        case -1:
+            computerScore++
+            break
+    }
+    playerCounter.textContent = `Player: ${playerScore}`
+    computerCounter.textContent = `Computer: ${computerScore}`
+    drawCounter.textContent = `Draw: ${drawScore}`
+}
+
+const checkScore = function() {
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore == 5) {
+            document.body.insertAdjacentHTML("beforeend", "<div>Player Won!!!</div>")
+        }
+        else if (computerScore == 5) {
+            document.body.insertAdjacentHTML("beforeend", "<div>Computer Won!!!</div>")
+        }
+
+        btns.forEach(btn => {
+            btn.removeEventListener('click', play)
+            btn.removeEventListener('click', checkScore)
+        })
+
     }
 }
 
-playGame()
+btns.forEach(btn => {
+    btn.addEventListener('click', play)
+    btn.addEventListener('click', checkScore)
+});
+
